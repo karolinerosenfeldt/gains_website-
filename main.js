18326c693997f4dde965c5899588f943d711b844
gcmpccnn <giacomopiccinini99@gmail.com>
2020-02-21 08:45:23 +0100

    fixed style a little bit and categories not working

--- main.js
+++ main.js
@@ -7,6 +7,26 @@ function getData(data) {
         .then(handleData);
 }
 
+
+function categories(data) {
+    data.forEach(function (cat) {
+        const link = document.createElement("a");
+        link.setAttribute("href", `gsx$category.$t`);
+        link.textContent = cat;
+        document.querySelector("nav").appendChild(link)
+
+        const section = document.createElement("section.exercise");
+        section.gsx$category.$t = cat;
+        const h4 = document.createElement("h4");
+        h4.textContent = cat;
+        document.querySelector("main").appendChild(h4);
+
+        document.querySelector("main").appendChild(section);
+    })
+    handleData();
+}
+
+
 function handleData(data) {
     const myData = data.feed.entry;
     console.log("myData - console");
@@ -21,9 +41,15 @@ function showData(singleData) {
     clone.querySelector("h2.exercise-muscle-group span").textContent = singleData.gsx$exercisemusclesgroup.$t;
     clone.querySelector("p.exercise-short-description").textContent = singleData.gsx$exerciseshortdescription.$t;
     clone.querySelector("h1.workout-name").textContent = singleData.gsx$workouttitle.$t;
-    clone.querySelector("h2.workout-duration").textContent = singleData.gsx$workoutduration.$t;
+    clone.querySelector("h2.workout-duration span").textContent = singleData.gsx$workoutduration.$t;
     clone.querySelector("p.workout-type").textContent = singleData.gsx$workouttype.$t;
     clone.querySelector("img.exercise-img").src = singleData.gsx$img.$t;
+    if (!singleData.gsx$workouttitle.$t) {
+        clone.querySelector("section.workouts").remove();
+    }
+    if (!singleData.gsx$exercisename.$t) {
+        clone.querySelector("section.exercise").remove();
+    }
 
 
     document.querySelector('main').appendChild(clone);
