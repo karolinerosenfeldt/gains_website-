3c4d928dfac36b81efd865c6df2c3996ef857311
Alli Bateman <allibateman@desktop-b3v5cno.kea.local>
2020-02-21 10:06:01 +0100

    categoreis not working - js12 data.forEach is not a function at createCategories

--- main.js
+++ main.js
@@ -2,55 +2,55 @@ const link = "https://spreadsheets.google.com/feeds/list/1lSLr0TScZbQtobbrCt3jJi
 window.addEventListener("DOMContentLoaded", getData);
 
 function getData(data) {
-    fetch(link)
-        .then(res => res.json())
-        .then(handleData);
-}
+    fetch("https://spreadsheets.google.com/feeds/list/1lSLr0TScZbQtobbrCt3jJiaKRwKJMfmT7e4UaktrN3A/od6/public/values?alt=json")
+        .then(function (response) {
+            return response.json()
+        })
+        .then(createCategories)
 
+    function createCategories(data) {
+        data.forEach(function (category) {
+            const link = document.createElement("a");
+            link.setAttribute("href", `#${category}`);
+            link.textContent = oneCat;
 
-function categories(data) {
-    data.forEach(function (cat) {
-        const link = document.createElement("a");
-        link.setAttribute("href", `gsx$category.$t`);
-        link.textContent = cat;
-        document.querySelector("nav").appendChild(link)
+            document.querySelector("nav").appendChild(link);
 
-        const section = document.createElement("section.exercise");
-        section.gsx$category.$t = cat;
-        const h4 = document.createElement("h4");
-        h4.textContent = cat;
-        document.querySelector("main").appendChild(h4);
-
-        document.querySelector("main").appendChild(section);
-    })
-    handleData();
-}
-
-
-function handleData(data) {
-    const myData = data.feed.entry;
-    console.log("myData - console");
-    console.log(myData);
-    myData.forEach(showData);
-}
-
-function showData(singleData) {
-    const template = document.querySelector('template').content;
-    const clone = template.cloneNode(true);
-    clone.querySelector("h1.exercise-name").textContent = singleData.gsx$exercisename.$t;
-    clone.querySelector("h2.exercise-muscle-group span").textContent = singleData.gsx$exercisemusclesgroup.$t;
-    clone.querySelector("p.exercise-short-description").textContent = singleData.gsx$exerciseshortdescription.$t;
-    clone.querySelector("h1.workout-name").textContent = singleData.gsx$workouttitle.$t;
-    clone.querySelector("h2.workout-duration span").textContent = singleData.gsx$workoutduration.$t;
-    clone.querySelector("p.workout-type").textContent = singleData.gsx$workouttype.$t;
-    clone.querySelector("img.exercise-img").src = singleData.gsx$img.$t;
-    if (!singleData.gsx$workouttitle.$t) {
-        clone.querySelector("section.workouts").remove();
+            const section = document.createElement("section.exercise");
+            section.id = category;
+            const h4 = document.createElement("h4");
+            h4.textContent = category;
+            document.querySelector("main").appendChild(section);
+        })
+        handleData();
     }
-    if (!singleData.gsx$exercisename.$t) {
-        clone.querySelector("section.exercise").remove();
+}
+
+
+    function handleData(data) {
+        const myData = data.feed.entry;
+        console.log("myData - console");
+        console.log(myData);
+        myData.forEach(showData);
     }
 
+    function showData(singleData) {
+        const template = document.querySelector('template').content;
+        const clone = template.cloneNode(true);
+        clone.querySelector("h1.exercise-name").textContent = singleData.gsx$exercisename.$t;
+        clone.querySelector("h2.exercise-muscle-group span").textContent = singleData.gsx$exercisemusclesgroup.$t;
+        clone.querySelector("p.exercise-short-description").textContent = singleData.gsx$exerciseshortdescription.$t;
+        clone.querySelector("h1.workout-name").textContent = singleData.gsx$workouttitle.$t;
+        clone.querySelector("h2.workout-duration span").textContent = singleData.gsx$workoutduration.$t;
+        clone.querySelector("p.workout-type").textContent = singleData.gsx$workouttype.$t;
+        clone.querySelector("img.exercise-img").src = singleData.gsx$img.$t;
+        if (!singleData.gsx$workouttitle.$t) {
+            clone.querySelector("section.workouts").remove();
+        }
+        if (!singleData.gsx$exercisename.$t) {
+            clone.querySelector("section.exercise").remove();
+        }
 
-    document.querySelector('main').appendChild(clone);
-}
+
+        document.querySelector('main').appendChild(clone);
+    }
