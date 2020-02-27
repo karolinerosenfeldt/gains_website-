a76dd6f79bcc513cd8f5f9e8bec3793a8bc48664
karoline rosenfeldt <karosenfeldt@gmail.com>
2020-02-27 12:46:38 +0100

    add ? to image urls

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ function showData(singleData) {
     clone.querySelector("h1.workout-name").textContent = singleData.gsx$workouttitle.$t;
     clone.querySelector("h2.workout-duration span").textContent = singleData.gsx$workoutduration.$t;
     clone.querySelector("p.workout-type").textContent = singleData.gsx$workouttype.$t;
-    clone.querySelector("img.exercise-img").src = singleData.gsx$img.$t;
+    clone.querySelector("img.exercise-img").src = singleData.gsx$img.$t+"?hi+mom";
     clone.querySelector("img.workout-img").src = singleData.gsx$img.$t;
     if (!singleData.gsx$workouttitle.$t) {
         clone.querySelector("article.one-workout").remove();
